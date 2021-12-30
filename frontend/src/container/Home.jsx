2e501e5188e6b2fd3e75fc65a9469f4041ab2032
Tayyab <heyitstay011@gmail.com>
2021-12-30 18:15:24 +0530

    Google Login

---
+++ frontend/src/container/Home.jsx
@@ -0,0 +1,13 @@
+import { HiMenu } from 'react-icons/hi';
+import { AiFillCloseCircle } from 'react-icons/ai';
+import { Link, Route, Routes } from 'react-router-dom';
+
+function Home(){
+    return (
+        <div className="">
+            Home
+        </div>
+    )
+}
+
+export default Home
