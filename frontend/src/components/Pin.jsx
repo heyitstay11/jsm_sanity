6d51e36052f523bae642fa75e3470f66b71b85ca
Tayyab <heyitstay011@gmail.com>
2022-01-02 12:09:56 +0530

    Create Pin

--- frontend/src/components/Pin.jsx
+++ frontend/src/components/Pin.jsx
@@ -32,6 +32,14 @@ const Pin = ({ pin : {postedBy, image, _id, destination, save} }) => {
         }
     }
 
+    const deletePin = (id) => {
+        client
+          .delete(id)
+          .then(() => {
+              window.location.reload();
+          })
+    }
+
     return (
         <div className="m-2">
             <div className="relative cursor-zoom-in w-auto hover:shadow-lg rounded-lg overflow-hidden transition-all duration-500 ease-in-out"
@@ -67,9 +75,36 @@ const Pin = ({ pin : {postedBy, image, _id, destination, save} }) => {
                                 </button>
                             )}
                         </div>
+                        <div className="flex justify-between items-center gap-2 w-full">
+                            {destination && (
+                                <a href={destination} className="bg-white flex items-center gap-2 text-black font-bold p-2 pl-4 pr-4 rounded-full opacity-70 hover:opacity-100 hover:shadow-md"
+                                target="_blank" rel="noreferrer" 
+                                >
+                                    <BsArrowUpRightCircleFill />
+                                    { destination.length > 16 ?  destination.slice(8,14) : destination.slice(8)}
+                                </a>
+                            )}
+                            {postedBy?._id === user.googleId && (
+                                <button className="bg-white opacity-70 hover:opacity-100 text-dark font-bold px-1 py-1 text-base rounded-3xl hover:shadow-md outline-none"
+                                onClick={(e) => {
+                                    e.stopPropagation();
+                                    deletePin(_id);
+                                }}>
+                                    <AiTwotoneDelete />
+                                </button>
+                            )}
+                        </div>
                     </div>
                 )}
             </div>
+            <Link to={`user-profile/${postedBy?._id}`} className="flex gap-2 mt-2 items-center">
+                  <img src={postedBy?.image} alt="user image"
+                  className="w-8 h-8 rounded-full object-cover"
+                   />
+                   <p className="font-semibold capitalize">
+                       {postedBy?.username}
+                   </p>
+            </Link>
         </div>
     )
 }
